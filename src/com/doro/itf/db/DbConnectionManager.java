582305e79103b1111e8d1675fedf6cbfa20f93d2
jangbyungmin <byungpal11@gmail.com>
2023-03-15 10:15:01 +0900

    s3

--- src/com/doro/itf/db/DbConnectionManager.java
+++ src/com/doro/itf/db/DbConnectionManager.java
@@ -35,7 +35,7 @@ public class DbConnectionManager {
     // dbconnection
     public Connection getConnection() throws SQLException {
 
-        return DriverManager.getConnection("jdbc:apache:commons:dbcp:/pool_test");
+        return DriverManager.getConnection("jdbc:apache:commons:dbcp:/pool_RevIFMain");
 
     }
 
